c7972fe369cb31f9a215edebe7271a60ac7c64a9
heidi suojanen <heidi.suojanen@edu.turkuamk.fi>
2026-04-10 17:22:38 +0300

    new cake

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ console.log("juustokakku onpi hyvää :P");
 console.log("niin on myäs morkkanapalakakku :)");
 console.log("Both. Both is good.");
 console.log("mansikkakakku on ihan jees myös");
+console.log("mmmmm mutakakku on ihanan suklaista");
